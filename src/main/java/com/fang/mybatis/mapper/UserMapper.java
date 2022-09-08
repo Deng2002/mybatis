b0c47412d656b22d467c1a145576d9b689d68e23
fang <2927109786@qq.com>
2022-09-08 10:16:39 +0800

    mybatis z,s,g,c-commit

--- src/main/java/com/fang/mybatis/mapper/UserMapper.java
+++ src/main/java/com/fang/mybatis/mapper/UserMapper.java
@@ -1,6 +1,30 @@
 package com.fang.mybatis.mapper;
 
-public interface UserMapper {
+import com.fang.mybatis.pojo.User;
 
+public interface UserMapper {
+    /**
+     * 添加用户
+     * @return
+     */
     int insertUser();
+
+    /**
+     * 更新用户
+     */
+    void updataUser();
+
+    /**
+     * 删除用户
+     */
+    void deleteUser();
+
+    /**
+     * 查询用户
+     */
+    User getUserByUsername(String username);
+    /**
+     * 验证用户
+     */
+    User checkUser(String username,String password);
 }
--- src/test/java/com/fang/mybatis/test/MybatisTest.java
+++ src/test/java/com/fang/mybatis/test/MybatisTest.java
@@ -1,6 +1,8 @@
 package com.fang.mybatis.test;
 
 import com.fang.mybatis.mapper.UserMapper;
+import com.fang.mybatis.pojo.User;
+import com.fang.mybatis.utils.SqlSessionUtil;
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
@@ -12,8 +14,7 @@ import java.io.InputStream;
 
 public class MybatisTest {
     @Test
-    public void textInsert() throws IOException {
-
+    public void testInsert() throws IOException {
 
         InputStream is = Resources.getResourceAsStream("mybatis-configs.xml");
 
@@ -27,6 +28,38 @@ public class MybatisTest {
         //提交事务
         sqlSession.commit();
         //关闭会话
+        sqlSession.close();
+    }
+    @Test
+    public void testUpdate(){
+        //获取会话
+        SqlSession sqlSession = SqlSessionUtil.getSqlSession();
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+        mapper.updataUser();
+        //结束会话
+        sqlSession.close();
+    }
+    @Test
+    public void testDelete(){
+        SqlSession sqlSession = SqlSessionUtil.getSqlSession();
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+        mapper.deleteUser();
+        sqlSession.close();
+    }
+    @Test
+    public void testQueryUserByUsername(){
+        SqlSession sqlSession = SqlSessionUtil.getSqlSession();
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+        User user = mapper.getUserByUsername("root");
+        System.out.println(user);
+        sqlSession.close();
+    }
+    @Test
+    public void testCheckUser(){
+        SqlSession sqlSession = SqlSessionUtil.getSqlSession();
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+        User user = mapper.checkUser("root", "1111");
+        System.out.println(user);
         sqlSession.close();
     }
 }
